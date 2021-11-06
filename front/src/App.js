0cc28b0aeee708c1adec16e1095410cae66316cc
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2021-11-06 17:34:43 +0100

    Update App component : protect with LoginGuard

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Row } from 'antd';
+import PrivatesRoutes from './config/route/routes/PrivatesRoutes';
+import LoginGuard from './config/guards/LoginGuard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <LoginGuard byPassLogin={false}>
+        <Row>
+          <PrivatesRoutes />
+        </Row>
+      </LoginGuard>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
